37c99c0c1206f77bb1e783aa874f49b9ced9ce01
林科 <linke@cocosurprise.com>
2018-02-26 19:30:02 +0800

    1.delete dist; 2.修改gitignore文件

--- src/service/api.js
+++ src/service/api.js
@@ -3,8 +3,12 @@ import { env } from '../config/config'
 const base = env.dev + 'ugc-xcx' // 基础路径
 
 const Api = {
-  // 用户
-  userApi: base + '/us',
+  /**
+   * 用户
+   * method post 创建用户
+   * method get 创建用户
+  */
+  userApi: base + '/us', 
 }
 
 module.exports = Api
